55e3d486edd008faebe2eb439c84952c44c36a00
yongrenwang <czie.alexmak@gmail.com>
2017-01-22 15:55:11 +0800

    Added a  variable to controller logic handling post requests; also, set the inptu fields to original values for cases where values have already been set previsouly and where user wants to update information

--- project.php
+++ project.php
@@ -11,13 +11,14 @@ if(isset($_GET['id'])){
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $project_id = filter_input(INPUT_POST,'id',FILTER_SANITIZE_NUMBER_INT);
     $title = filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING);
     $category = filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING);
 
     if(empty($title) || empty($category)){
         $error_message = "Please fill in the required fields: Title, Category";
     }else{
-        if(add_project($title,$category)){
+        if(add_project($title,$category,$project_id)){
             header("Location: project_list.php");
             exit;
         }else{
@@ -55,18 +56,38 @@ include 'inc/header.php';
                 <table>
                     <tr>
                         <th><label for="title">Title<span class="required">*</span></label></th>
-                        <td><input type="text" id="title" name="title" value="" /></td>
+                        <td><input type="text" id="title" name="title" value="<?php echo $title; ?>" /></td>
                     </tr>
                     <tr>
                         <th><label for="category">Category<span class="required">*</span></label></th>
                         <td><select id="category" name="category">
                                 <option value="">Select One</option>
-                                <option value="Billable">Billable</option>
-                                <option value="Charity">Charity</option>
-                                <option value="Personal">Personal</option>
+                                <option value="Billable"
+                                    <?php
+                                        if($category == "Billable"){
+                                            echo 'selected';
+                                        }
+                                    ?>>Billable</option>
+                                <option value="Charity" <?php
+                                        if($category == "Charity"){
+                                            echo 'selected';
+                                        }
+                                    ?>>Charity</option>
+                                <option value="Personal" <?php
+                                        if($category == "Personal"){
+                                            echo 'selected';
+                                        }
+                                    ?>>Personal</option>
                         </select></td>
                     </tr>
                 </table>
+                <?php
+
+                    if(!empty($project_id)){
+                        echo '<input type="hidden" name="id" value="'. $project_id .'"/>';
+                    }
+
+                ?>
                 <input class="button button--primary button--topic-php" type="submit" value="Submit" />
             </form>
         </div>
