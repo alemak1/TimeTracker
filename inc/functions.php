40d4de5afcadd2b032163213b48809793688aab2
yongrenwang <czie.alexmak@gmail.com>
2017-01-22 17:04:02 +0800

    Added a new delete_task function to functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -160,3 +160,22 @@ function get_task($task_id){
 
 }
 
+function delete_task($task_id){
+	include 'connection.php';
+
+	$sql = 'DELETE FROM tasks WHERE task_id = ?';
+
+	try{
+		$results = $db->prepare($sql);
+		$results->bindValue(1,$task_id,PDO::PARAM_INT);
+		$results->execute();
+
+	}catch(Exception $e){
+		echo "Error: ". $e->getMessage() . "<br />";
+		return false;
+	}
+
+	return true;
+
+}
+
