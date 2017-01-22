7835f7d71149150849c2fa77044d02c97ce3d8e6
yongrenwang <czie.alexmak@gmail.com>
2017-01-22 14:23:37 +0800

    Refactored get_task_list() function to allow for filtering by date

--- inc/functions.php
+++ inc/functions.php
@@ -30,6 +30,9 @@ function get_task_list($filter = null){
 			case 'category':
 				$where = ' WHERE category = ?';
 				break;
+			case 'date':
+				$where = ' WHERE date >= ? AND date <= ?';
+				break;
 
 		}
 	}
@@ -44,6 +47,9 @@ function get_task_list($filter = null){
 		$results = $db->prepare($sql . $where . $orderBy);
 		if(is_array($filter)){
 			$results->bindValue(1,$filter[1]);
+			if($filter[0] == 'date'){
+				$results->bindValue(2,$filter[2], PDO::PARAM_STR);
+			}
 		}
 		$results->execute();
 	}catch(Exception $e){
